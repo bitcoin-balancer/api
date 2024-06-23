b9033c1d6ddfcb5fd9d923cf9af85d72d4adb086
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-06-23 12:32:48 -0400

    Initialize express v5
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('Balancer API');
+import express from 'express';
+
+/**
+ * Express Application
+ * ...
+ */
+const app = express();
+
+
+
+/**
+ * Routes
+ * ...
+ */
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+
+
+/**
+ * Server Initialization
+ * ...
+ */
+const server = app.listen(5075);
+console.log('Balancer API Initialized');
+console.log('Running: v1.0.0');
+console.log('Port: 5075');
+console.log('Production: false');
