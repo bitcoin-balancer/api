e8905fae7af04f3b9ccd1524fe100448e42e71cf
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-08-23 08:59:35 -0400

    Initialize Binance's ticker type
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/modules/shared/exchange/binance/types.ts
+++ src/modules/shared/exchange/binance/types.ts
@@ -66,6 +66,41 @@ type IBinanceCandlestick = [
 
 
 /* ************************************************************************************************
+ *                                           ORDER BOOK                                           *
+ ************************************************************************************************ */
+
+
+
+
+
+/* ************************************************************************************************
+ *                                             TICKER                                             *
+ ************************************************************************************************ */
+
+/**
+ * Coin Ticker
+ * The 24 hour rolling window price change statistics.
+ */
+type ICoinTicker = {
+  symbol: string; // trading pair                                   e.g. "ETHUSDT"
+  openPrice: string; // the price 24h ago                           e.g. "2625.00000000"
+  highPrice: string; // the highest price in the last 24h           e.g. "2689.00000000"
+  lowPrice: string; // the lowest price in the last 24h             e.g. "2590.50000000"
+  lastPrice: string; // the last traded price                       e.g. "2665.81000000"
+  volume: string; // the 24h volume in base asset                   e.g. "239064.08200000"
+  quoteVolume: string; // the 24h volume in quote asset             e.g. "631427670.76268000"
+  openTime: number; // the timestamp of the beginning of the ticker e.g. 1724331055499
+  closeTime: number; // the time at which the ticket will close     e.g. 1724417455499
+  firstId: number; // trade ID of the first trade in the interval   e.g. 1538342959
+  lastId: number; // trade ID of the last trade in the interval     e.g. 1539402424
+  count: number; // the number of trades in the interval            e.g. 1059466
+};
+
+
+
+
+
+/* ************************************************************************************************
  *                                         MODULE EXPORTS                                         *
  ************************************************************************************************ */
 export type {
@@ -75,4 +110,10 @@ export type {
   // candlestick
   IBinanceCandlestickInterval,
   IBinanceCandlestick,
+
+  // order book
+  // ...
+
+  // ticket
+  ICoinTicker,
 };
