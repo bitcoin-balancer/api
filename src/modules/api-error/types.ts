577543d5e130cfa97ec9d8927cfd018df94f5331
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-09-11 15:31:28 -0400

    Plug the Strategy into the PositionRouter
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/modules/api-error/types.ts
+++ src/modules/api-error/types.ts
@@ -55,7 +55,8 @@ type IAPIErrorOrigin = 'AutomatedTest' | 'Notification.broadcast' | 'APIErrorRou
 | 'MarketStateRouter.get.liquidity.state' | 'MarketStateRouter.get.liquidity.config' | 'MarketStateRouter.put.liquidity.config'
 | 'MarketStateRouter.get.reversal.config' | 'MarketStateRouter.put.reversal.config' | 'MarketStateRouter.get.reversal.record'
 | 'MarketStateRouter.get.reversal.records' | 'OrderBook.interval.refetch' | 'CandlestickRouter.get.event-history'
-| 'BalanceService.__refetchInterval' | 'PositionRouter.get.balances';
+| 'BalanceService.__refetchInterval' | 'PositionRouter.get.balances' | 'PositionRouter.get.strategy'
+| 'PositionRouter.put.strategy';
 
 /**
  * API Error
--- src/modules/position/router.ts
+++ src/modules/position/router.ts
@@ -1,11 +1,65 @@
 import { Router, Request, Response } from 'express';
 import { buildResponse } from 'api-response-utils';
-import { lowRiskLimit } from '../../middlewares/rate-limit/index.js';
+import { lowRiskLimit, mediumRiskLimit } from '../../middlewares/rate-limit/index.js';
 import { checkRequest } from '../shared/request-guard/index.js';
 import { APIErrorService } from '../api-error/index.js';
+import { StrategyService } from './strategy/index.js';
 import { BalanceService } from './balance/index.js';
 
+
 const PositionRouter = Router();
+
+/* ************************************************************************************************
+ *                                            STRATEGY                                            *
+ ************************************************************************************************ */
+
+/**
+ * Retrieves the strategy.
+ * @returns IAPIResponse<IStrategy>
+ * @requirements
+ * - authority: 2
+ */
+PositionRouter.route('/strategy').get(lowRiskLimit, async (req: Request, res: Response) => {
+  let reqUid: string | undefined;
+  try {
+    reqUid = await checkRequest(req.get('authorization'), req.ip, 2);
+    res.json(buildResponse(StrategyService.config));
+  } catch (e) {
+    APIErrorService.save('PositionRouter.get.strategy', e, reqUid, req.ip);
+    res.json(buildResponse(undefined, e));
+  }
+});
+
+/**
+ * Updates the strategy.
+ * @param newConfig
+ * @returns IAPIResponse<void>
+ * @requirements
+ * - authority: 3
+ * - otp-token
+ */
+PositionRouter.route('/strategy').put(mediumRiskLimit, async (req: Request, res: Response) => {
+  let reqUid: string | undefined;
+  try {
+    reqUid = await checkRequest(
+      req.get('authorization'),
+      req.ip,
+      3,
+      ['newConfig'],
+      req.body,
+      req.get('otp-token') || '',
+    );
+    await StrategyService.updateConfiguration(req.body.newConfig);
+    res.json(buildResponse());
+  } catch (e) {
+    APIErrorService.save('PositionRouter.put.strategy', e, reqUid, req.ip, req.body);
+    res.json(buildResponse(undefined, e));
+  }
+});
+
+
+
+
 
 /* ************************************************************************************************
  *                                            BALANCES                                            *
