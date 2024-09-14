262afbc51f417473e690b0b805808469507a43f2
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-09-14 13:09:02 -0400

    Complete the initial implementation of the TransactionModel
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/modules/position/transaction/model.ts
+++ src/modules/position/transaction/model.ts
@@ -2,7 +2,119 @@ import { DatabaseService } from '../../database/index.js';
 import { ITransaction } from './types.js';
 
 /* ************************************************************************************************
- *                                         IMPLEMENTATION                                         *
+ *                                           RETRIEVERS                                           *
+ ************************************************************************************************ */
+
+/**
+ * Retrieves a record by ID. If the record is not found it returns undefined.
+ * @param id
+ * @returns Promise<ITransaction | undefined>
+ */
+const getTransactionRecord = async (id: number): Promise<ITransaction | undefined> => {
+  const { rows } = await DatabaseService.pool.query({
+    text: `
+      SELECT id, event_time, status, side, amount, logs
+      FROM ${DatabaseService.tn.transactions}
+      WHERE id = $1;
+    `,
+    values: [id],
+  });
+  return rows[0];
+};
+
+/**
+ * Retrieves a list of existing Transactions constrained by a limit.
+ * @param limit
+ * @returns Promise<ITransaction[]>
+ */
+const __listTransactionRecords = async (limit: number): Promise<ITransaction[]> => {
+  const { rows } = await DatabaseService.pool.query({
+    text: `
+      SELECT id, event_time, status, side, amount, logs
+      FROM ${DatabaseService.tn.transactions}
+      ORDER BY id DESC
+      LIMIT $1;
+    `,
+    values: [limit],
+  });
+  return rows;
+};
+
+/**
+ * Retrieves the list of Transactions starting at the given point. Note: the startAtID record will
+ * not be included in the result.
+ * @param limit
+ * @param startAtID
+ * @returns Promise<ITransaction[]>
+ */
+const __listNextTransactionRecords = async (
+  limit: number,
+  startAtID: number,
+): Promise<ITransaction[]> => {
+  const { rows } = await DatabaseService.pool.query({
+    text: `
+      SELECT id, event_time, status, side, amount, logs
+      FROM ${DatabaseService.tn.transactions}
+      WHERE id < $1
+      ORDER BY id DESC
+      LIMIT $2;
+    `,
+    values: [startAtID, limit],
+  });
+  return rows;
+};
+
+/**
+ * Retrieves a list of Transactions from the database. If a startAtID is provided, it will only
+ * retrieve records that are older than the passed ID (exclusive).
+ * @param limit
+ * @param startAtID?
+ * @returns Promise<ITransaction[]>
+ */
+const listTransactionRecords = (limit: number, startAtID?: number): Promise<ITransaction[]> => (
+  typeof startAtID === 'number'
+    ? __listNextTransactionRecords(limit, startAtID)
+    : __listTransactionRecords(limit)
+);
+
+/**
+ * Retrieves a list of Transactions that are between a date range.
+ * @param startAt
+ * @param endAt?
+ * @returns Promise<ITransaction[]>
+ */
+const listTransactionRecordsByRange = async (
+  startAt: number,
+  endAt?: number,
+): Promise<ITransaction[]> => {
+  // init values
+  let text: string = `
+    SELECT id, event_time, status, side, amount, logs
+    FROM ${DatabaseService.tn.transactions}
+    WHERE event_time >= $1
+  `;
+  const values: number[] = [startAt];
+
+  // include the conditional clause based on params
+  if (typeof endAt === 'number') {
+    text += ' AND event_time <= $2';
+    values.push(endAt);
+  }
+
+  // order the records
+  text += ' ORDER BY event_time ASC;';
+
+  // execute the query and return the results
+  const { rows } = await DatabaseService.pool.query({ text, values });
+  return rows;
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                            ACTIONS                                             *
  ************************************************************************************************ */
 
 /**
@@ -46,6 +158,12 @@ const updateTransactionRecord = async (tx: ITransaction): Promise<void> => {
  *                                         MODULE EXPORTS                                         *
  ************************************************************************************************ */
 export {
+  // retrievers
+  getTransactionRecord,
+  listTransactionRecords,
+  listTransactionRecordsByRange,
+
+  // actions
   createTransactionRecord,
   updateTransactionRecord,
 };
