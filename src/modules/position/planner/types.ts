da2c22b806905594d5fba0b8af5adbf88acba671
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-11-15 15:18:47 -0400

    Init position action plan types
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/modules/position/planner/types.ts
+++ src/modules/position/planner/types.ts
@@ -5,12 +5,29 @@
  ************************************************************************************************ */
 
 /**
+ * Increase Plan
+ * ..
+ */
+type IIncreasePlan = {
+
+};
+
+/**
+ * Decrease Plan
+ * ..
+ */
+type IDecreasePlan = {
+
+};
+
+/**
  * Position Plan
  * The object which describes how Balancer will react based on the state of the market and the
  * strategy.
  */
 type IPositionPlan = {
-
+  increase: IIncreasePlan;
+  decrease: IDecreasePlan;
 };
 
 
@@ -21,5 +38,7 @@ type IPositionPlan = {
  *                                         MODULE EXPORTS                                         *
  ************************************************************************************************ */
 export type {
+  IIncreasePlan,
+  IDecreasePlan,
   IPositionPlan,
 };
