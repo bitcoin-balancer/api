c09932de8dfc2833d681b4791fa6aa9a50f5352d
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-11-30 12:21:01 -0400

    Re-add the uid_token_idx index as it is used by the sign out functionality
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/modules/database/raw-tables.ts
+++ src/modules/database/raw-tables.ts
@@ -65,6 +65,7 @@ export const RAW_TABLES: IRawTable[] = [
         event_time  BIGINT NOT NULL
       );
       CREATE INDEX IF NOT EXISTS ${getTableName('refresh_tokens')}_uid_idx ON ${getTableName('refresh_tokens')}(uid);
+      CREATE INDEX IF NOT EXISTS ${getTableName('refresh_tokens')}_uid_token_idx ON ${getTableName('refresh_tokens')}(uid, token);
       CREATE INDEX IF NOT EXISTS ${getTableName('refresh_tokens')}_uid_event_time_idx ON ${getTableName('refresh_tokens')}(uid, event_time DESC);`,
   },
 
